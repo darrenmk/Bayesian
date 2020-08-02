cc1961afcd47d0ef588cdc2517a7ef6615d1cc8e
darrenmk <40967253+darrenmk@users.noreply.github.com>
2020-08-02 19:07:05 -0400

    Update BinomialModelComparison1.py

--- ModelComparison/BinomialModelComparison1.py
+++ ModelComparison/BinomialModelComparison1.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-Hpc = np.arange(0.05,1,.1)
-HmacA = Hpc*(1/5)
-HmacB = Hpc*(2/5)
+Ho1 = np.arange(0.05,1,.1)
+Ho2A = Ho1*(1/5)
+Ho2B = Ho1*(2/5)
 
 o1 = [20,50]
 o2 = [5,65]
 
-likeM1 = np.mean([((p**o1[0])*((1-p)**o1[1]))*((p**o2[0])*((1-p)**o2[1])) for p in Hpc])
-likeM2 = np.mean([((p1**o1[0])*((1-p1)**o1[1])*(p2**o2[0])*((1-p2)**o2[1])) for p1,p2 in zip(Hpc,HmacA)] + [((p1**o1[0])*((1-p1)**o1[1])*(p2**o2[0])*((1-p2)**o2[1])) for p1,p2 in zip(Hpc,HmacB)])
+likeM1 = np.mean([((p**o1[0])*((1-p)**o1[1]))*((p**o2[0])*((1-p)**o2[1])) for p in Ho1])
+likeM2 = np.mean([((p1**o1[0])*((1-p1)**o1[1])*(p2**o2[0])*((1-p2)**o2[1])) for p1,p2 in zip(Ho1,Ho2A)] + [((p1**o1[0])*((1-p1)**o1[1])*(p2**o2[0])*((1-p2)**o2[1])) for p1,p2 in zip(Ho1,Ho2B)])
 
 postM1 = likeM1/(likeM1+likeM2)
 postM2 = likeM2/(likeM1+likeM2)
