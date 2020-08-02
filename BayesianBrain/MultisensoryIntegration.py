27563461e78bf098a72f6fb5e23f9c6ea736a337
darrenmk <40967253+darrenmk@users.noreply.github.com>
2020-08-02 18:22:16 -0400

    Update MultisensoryIntegration.py

--- BayesianBrain/MultisensoryIntegration.py
+++ BayesianBrain/MultisensoryIntegration.py
@@ -9,20 +9,20 @@ xa = .1
 sigmav = 1.5
 sigmaa = 2
 
-likev = [(1/(sigmav*math.sqrt(2*math.pi)))*math.exp((-(xv-s)**2)/(2*(sigmav**2))) for s in xpos]
-likea = [(1/(sigmaa*math.sqrt(2*math.pi)))*math.exp((-(xa-s)**2)/(2*(sigmaa**2))) for s in xpos]
+Lv = [(1/(sigmav*math.sqrt(2*math.pi)))*math.exp((-(xv-s)**2)/(2*(sigmav**2))) for s in xpos] # likelihood function of visual estimate
+La = [(1/(sigmaa*math.sqrt(2*math.pi)))*math.exp((-(xa-s)**2)/(2*(sigmaa**2))) for s in xpos] # likelihood function of auditory estimate
 
-likec = [a*v for a,v in zip(likea,likev)]
-likec1 = [(x/sum(likec))/.01 for x in likec]
+Lc = [a*v for a,v in zip(La,Lv)] # likelihood function of combined estimate
+Lc1 = [(x/sum(Lc))/.01 for x in Lc] # likelihood function of combined estimate
 
 mu = ((xv/(sigmav**2))+(xa/(sigmaa**2)))/((1/(sigmav**2))+(1/(sigmaa**2)))
 var = 1/((1/(sigmav**2))+(1/(sigmaa**2)))
 
-likec2 = [(1/(math.sqrt(var)*math.sqrt(2*math.pi)))*math.exp((-(mu-s)**2)/(2*var)) for s in xpos]
+Lc2 = [(1/(math.sqrt(var)*math.sqrt(2*math.pi)))*math.exp((-(mu-s)**2)/(2*var)) for s in xpos]
 
 plt.axes(xticks=np.arange(0,1001,100), xticklabels=np.arange(-5,5,1))
 plt.plot(likev,color='blue')
 plt.plot(likea,color='red')
 plt.plot(likec1,color='green')
 
-mode = [xpos[likec1.index(max(likec1))],max(likec1)]
+MAP = [xpos[Lc2.index(max(Lc2))],max(Lc2)]
