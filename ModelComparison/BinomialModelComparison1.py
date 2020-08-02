225fa1bfb9d341ac54f3a6d7e77ebd82c6c5a6db
darrenmk <40967253+darrenmk@users.noreply.github.com>
2020-08-02 19:12:20 -0400

    Update BinomialModelComparison1.py

--- ModelComparison/BinomialModelComparison1.py
+++ ModelComparison/BinomialModelComparison1.py
@@ -4,8 +4,8 @@ Ho1 = np.arange(0.05,1,.1)
 Ho2A = Ho1*(1/5)
 Ho2B = Ho1*(2/5)
 
-o1 = [20,50]
-o2 = [5,65]
+o1 = [n1,k-n1] # fill in values
+o2 = [n2,k-n2] # fill in values
 
 likeM1 = np.mean([((p**o1[0])*((1-p)**o1[1]))*((p**o2[0])*((1-p)**o2[1])) for p in Ho1])
 likeM2 = np.mean([((p1**o1[0])*((1-p1)**o1[1])*(p2**o2[0])*((1-p2)**o2[1])) for p1,p2 in zip(Ho1,Ho2A)] + [((p1**o1[0])*((1-p1)**o1[1])*(p2**o2[0])*((1-p2)**o2[1])) for p1,p2 in zip(Ho1,Ho2B)])
