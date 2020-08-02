5416096fd650c1b9d3d03dfbc6ab7c922333a6c1
darrenmk <40967253+darrenmk@users.noreply.github.com>
2020-08-02 18:13:32 -0400

    Update NeuronTuningCurve.py

--- BayesianBrain/NeuronTuningCurve.py
+++ BayesianBrain/NeuronTuningCurve.py
@@ -3,10 +3,9 @@ import math
 
 Htheta = np.arange(1,181)
 L = np.zeros((180))
-thetaHat = [80,90,100]
-n = [41,34,16]
+thetaHat = [0,0,0] # neuron tuning curves
+n = [0,0,0] # neuron n spikes
 
-# Q1b
 for k in range(len(Htheta)):
     rate = [50*math.exp((-(Htheta[k]-thetaHat[t])**2)/(2*(20**2))) for t in range(len(thetaHat))]
     L[k] = np.prod([(math.exp(-rate)*(rate**n))/math.factorial(n) for rate,n in zip(rate,n)])
@@ -26,5 +25,3 @@ while x < 0.95:
 
 idx = [x for x in idx if x is not None]
 confidenceInterval = [min(idx),max(idx)]
-
-# Q1c: because the actual number of spikes were drawn from a Poisson distribution where r is the rate
