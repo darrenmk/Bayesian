490eaf8e525dd8054bb6a69a5dc32d96cd16fbd0
darrenmk <40967253+darrenmk@users.noreply.github.com>
2020-08-02 19:15:06 -0400

    Update RobustGaussian.py

--- RobustEstimation/RobustGaussian.py
+++ RobustEstimation/RobustGaussian.py
@@ -5,14 +5,14 @@ import math
 # Q3a
 Hmu = np.arange(30,50.1,1)
 Hsigma = np.arange(1,10.1,0.45)
-speed = [42,38,44,43,44]
+s = [42,38,44,43,44]
 La = np.zeros((21,21))
 
 for i in range(len(Hmu)):
     for j in range(len(Hsigma)):
         mu = round(Hmu[i],2)
         sigma = round(Hsigma[j],2)
-        Px = [(1/sigma*math.sqrt(2*math.pi))*math.exp(-((x-mu)**2/(2*sigma**2))) for x in speed]
+        Px = [(1/sigma*math.sqrt(2*math.pi))*math.exp(-((x-mu)**2/(2*sigma**2))) for x in s]
         La[i,j] = np.prod(Px)
 
 jPDFa = La/La.sum() # uniform priors
@@ -28,14 +28,14 @@ modea = [Hmu[muPDFa.index(max(muPDFa))],max(muPDFa)]
 
 
 # Q3b
-speed2 = [42,38,44,43,44,30]
+s2 = [42,38,44,43,44,30]
 Lb = np.zeros((21,21))
 
 for i in range(len(Hmu)):
     for j in range(len(Hsigma)):
         mu = round(Hmu[i],2)
         sigma = round(Hsigma[j],2)
-        Px = [(1/sigma*math.sqrt(2*math.pi))*math.exp(-((x-mu)**2/(2*sigma**2))) for x in speed2]
+        Px = [(1/sigma*math.sqrt(2*math.pi))*math.exp(-((x-mu)**2/(2*sigma**2))) for x in s2]
         Lb[i,j] = np.prod(Px)
 
 jPDFb = Lb/Lb.sum() # uniform priors
@@ -57,7 +57,7 @@ for i in range(len(Hmu)):
     for j in range(len(Hsigma)):
         mu = round(Hmu[i],2)
         sigma = round(Hsigma[j],2)
-        Px = [0.1 + 0.9*(1/sigma*math.sqrt(2*math.pi))*math.exp(-((x-mu)**2/(2*sigma**2))) for x in speed2]
+        Px = [0.1 + 0.9*(1/sigma*math.sqrt(2*math.pi))*math.exp(-((x-mu)**2/(2*sigma**2))) for x in s2]
         Lc[i,j] = np.prod(Px)
 
 jPDFc = Lc/Lc.sum() # uniform priors
